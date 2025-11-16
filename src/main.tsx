cfce8a0770d0a2d091c53bd4041ac72d3b1aea30
Kaffahh <kaffah505@gmail.com>
2025-11-16 08:40:28 +0700

    Delete temulokal on base

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/temulokal/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
